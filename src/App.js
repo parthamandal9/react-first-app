fa20c795b5c36e720a03ded31d867cb0462065e9
Partha Mandal <79751124+ParthaGrowwFaster@users.noreply.github.com>
2021-12-18 15:12:33 +0530

    Passing Dynamic Props.

--- src/App.js
+++ src/App.js
@@ -2,28 +2,36 @@ import React from "react";
 import Profile from "./components/Profile";
 
 const App = () => {
+  const dilshadSuper = true;
+  const parthaSuper = false;
+  const shakilSuper = false;
+  const deepakSuper = true;
+
   return (
     <div>
       <Profile
         name="Dilshad"
         linktoGithub="https://www.github.com/webformulator"
         about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
-        isSuperUser={true}
+        isSuperUser={dilshadSuper}
       />
       <Profile
         name="Partha"
         linktoGithub="https://www.github.com/webformulator"
         about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
+        isSuperUser={parthaSuper}
       />
       <Profile
         name="Shakil"
         linktoGithub="https://www.github.com/webformulator"
         about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
+        isSuperUser={shakilSuper}
       />
       <Profile
         name="Deepak"
         linktoGithub="https://www.github.com/webformulator"
         about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
+        isSuperUser={deepakSuper}
       />
     </div>
   );
