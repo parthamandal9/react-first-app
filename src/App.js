981c07e973c7913f99e70200b4fa36fb937e5f46
Partha Mandal <79751124+ParthaGrowwFaster@users.noreply.github.com>
2021-12-18 15:24:39 +0530

    Rendering List of Users.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
 import React from "react";
+import users from "./data/users.json";
 import Profile from "./components/Profile";
 
 const App = () => {
-  const dilshadSuper = true;
-  const parthaSuper = false;
-  const shakilSuper = false;
-  const deepakSuper = true;
-
   return (
     <div>
-      <Profile
-        name="Dilshad"
-        linktoGithub="https://www.github.com/webformulator"
-        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
-        isSuperUser={dilshadSuper}
-      />
-      <Profile
-        name="Partha"
-        linktoGithub="https://www.github.com/webformulator"
-        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
-        isSuperUser={parthaSuper}
-      />
-      <Profile
-        name="Shakil"
-        linktoGithub="https://www.github.com/webformulator"
-        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
-        isSuperUser={shakilSuper}
-      />
-      <Profile
-        name="Deepak"
-        linktoGithub="https://www.github.com/webformulator"
-        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
-        isSuperUser={deepakSuper}
-      />
+      {users.map(({ name, about, linktoGithub, isSuperUser }) => (
+        <Profile
+          name={name}
+          about={about}
+          linktoGithub={linktoGithub}
+          isSuperUser={isSuperUser}
+        />
+      ))}
     </div>
   );
 };
