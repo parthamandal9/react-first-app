2f72a52155e9886ae5917bc8612634b7e683d499
Partha Mandal <79751124+ParthaGrowwFaster@users.noreply.github.com>
2021-12-18 14:59:44 +0530

    Passing Props.

--- src/App.js
+++ src/App.js
@@ -4,48 +4,26 @@ import Profile from "./components/Profile";
 const App = () => {
   return (
     <div>
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <div>
-        <h3>Md Dilshad Alam</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-          placeat nobis iure distinctio, repellat quidem laudantium quas ab
-          inventore iste doloremque repellendus, totam accusantium commodi
-          fugit! Perferendis ipsum et, quae officia aliquid quos cumque
-          exercitationem animi eligendi sint voluptatem sunt laudantium nobis,
-          alias ratione repellat sed aliquam ex quisquam? Cupiditate.
-        </p>
-        <a href="https://www.github.com/webformulator">Github</a>
-      </div>
-      <div>
-        <h3>Partha Mandal</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-          placeat nobis iure distinctio, repellat quidem laudantium quas ab
-          inventore iste doloremque repellendus, totam accusantium commodi
-          fugit! Perferendis ipsum et, quae officia aliquid quos cumque
-          exercitationem animi eligendi sint voluptatem sunt laudantium nobis,
-          alias ratione repellat sed aliquam ex quisquam? Cupiditate.
-        </p>
-        <a href="https://www.github.com/partha">Github</a>
-      </div>
-      <div>
-        <h3>Shakil Ahmad</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-          placeat nobis iure distinctio, repellat quidem laudantium quas ab
-          inventore iste doloremque repellendus, totam accusantium commodi
-          fugit! Perferendis ipsum et, quae officia aliquid quos cumque
-          exercitationem animi eligendi sint voluptatem sunt laudantium nobis,
-          alias ratione repellat sed aliquam ex quisquam? Cupiditate.
-        </p>
-        <a href="https://www.github.com/shakil">Github</a>
-      </div>
+      <Profile
+        name="Dilshad"
+        linktoGithub="https://www.github.com/webformulator"
+        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
+      />
+      <Profile
+        name="Partha"
+        linktoGithub="https://www.github.com/webformulator"
+        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
+      />
+      <Profile
+        name="Shakil"
+        linktoGithub="https://www.github.com/webformulator"
+        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
+      />
+      <Profile
+        name="Deepak"
+        linktoGithub="https://www.github.com/webformulator"
+        about="Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate."
+      />
     </div>
   );
 };
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-function Profile() {
-  const name = "Partha Mandal";
-  const linktoGithub = "https://www.github.com/webformulator";
-  const about =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam placeat  nobis iure distinctio, epellat quidem laudantium quas ab inventore iste  doloremque repellendus, totam accusantium commodi fugit! Perferendis  ipsum et, quae officia aliquid quos cumque exercitationem animi eligendi  sint voluptatem sunt laudantium nobis, alias ratione repellat sed  aliquam ex quisquam? Cupiditate.";
+function Profile(props) {
+  const name = props.name;
+  const linktoGithub = props.linktoGithub;
+  const about = props.about;
   return (
     <div>
       <h3>{name}</h3>
