f5d90b2441681abaf6b56cf3fb15baed2b37e828
Partha Mandal <79751124+ParthaGrowwFaster@users.noreply.github.com>
2021-12-19 16:40:15 +0530

    Handling Input Text. way 2

--- src/components/react-form/ReactForm.jsx
+++ src/components/react-form/ReactForm.jsx
@@ -4,17 +4,13 @@ const ReactForm = () => {
   const [formData, setFormData] = useState({});
   const [name, setName] = useState("");
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-  };
-
   return (
     <>
       <h1>React Form Inputs</h1>
       <input
         type="text"
         name="name"
-        onChange={(event) => handleChange(event)}
+        onChange={(event) => setName(event.target.value)}
       />
       <p>Name : {name}</p>
     </>
