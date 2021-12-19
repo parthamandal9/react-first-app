182390630235d0cea375902b08386d5a6eda455b
Partha Mandal <79751124+ParthaGrowwFaster@users.noreply.github.com>
2021-12-19 16:13:36 +0530

    Creation of State in Functional Component.

--- src/components/react-state/ReactState.jsx
+++ src/components/react-state/ReactState.jsx
@@ -1,26 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ReactState extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { data: ["Dilshad", "Partha", "Shakil"] };
-    this.changeState = this.changeState.bind(this);
-  }
-  changeState() {
-    this.setState({ name: "Partha" });
-  }
-  render() {
-    return (
-      <>
-        <h1>React State</h1>
-        <ul>
-          {this.state.data.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+const ReactState = (props) => {
+  const [name] = useState("Dilshad");
+
+  return (
+    <>
+      <h1>React State</h1>
+      <p>Name : {name}</p>
+    </>
+  );
+};
 
 export default ReactState;
