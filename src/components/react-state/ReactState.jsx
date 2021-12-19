f635f7eeee12c3166cbc59d8a9790b9f83fc52c6
Partha Mandal <79751124+ParthaGrowwFaster@users.noreply.github.com>
2021-12-19 16:22:14 +0530

    Array of items in functional state.

--- src/components/react-state/ReactState.jsx
+++ src/components/react-state/ReactState.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 
+const items = [
+  { name: "Dilshad", mob: 6201419054 },
+  { name: "Partha", mob: 8250761658 },
+];
+
 const ReactState = (props) => {
-  const [name, setName] = useState("Dilshad");
+  const [data, setData] = useState(items);
 
   const changeState = () => {
-    setName("Partha");
+    setData([
+      { name: "Shakil", mob: 6201419054 },
+      { name: "Deepak", mob: 8250761658 },
+    ]);
   };
 
   return (
     <>
       <h1>React State</h1>
-      <p onClick={changeState}>Name : {name}</p>
+      <ul onClick={changeState}>
+        {data.map((item) => (
+          <li key={item.name}>Name : {item.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
